eb72792d1ffbdd7487753d0b005d37506424dd86
Patrick Punch <punchp@oregonstate.edu>
2025-03-17 13:00:53 -0700

    added item testing to item_spec.rb

--- lib/item.rb
+++ lib/item.rb
@@ -2,6 +2,7 @@ class Item
   attr_reader :name, :volume
 
   def initialize(name, volume)
+    raise ArgumentError, 'Volume must be > 0' if volume <= 0
     @name = name
     @volume = volume
   end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,20 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
+  it 'has a name and volume' do
+    item = Item.new('Soda', 500)
+    expect(item.name).to eq('Soda')
+    expect(item.volume).to eq(500)
+  end
 
+  it 'is not equal to another item with different name or volume' do
+    item1 = Item.new('Soda', 500)
+    item2 = Item.new('Noodles', 1300)
+    expect(item1).not_to eq(item2)
+  end
+
+  it 'raises an error if volume is negative or zero' do
+    expect { Item.new('Soda', -100) }.to raise_error(ArgumentError)
+    expect { Item.new('Water', 0) }.to raise_error(ArgumentError)
+  end
 end
